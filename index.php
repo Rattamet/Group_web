cabeca7bc983bdfbb8d0a89688fd3263d4c4b443
Rattamet <76036883+Rattamet@users.noreply.github.com>
2020-12-22 01:09:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="th">
+    <head>
+        <style>
+            body {
+                background-color: #1f1f7e;
+            }
+            #block_container {
+                display: flex;
+                
+            } 
+            #bloc1{
+                margin: auto;
+                padding: 30px; 
+                background-color:#3e4e41; 
+                width: 600px;
+                height: 200px;
+                font-family:monaco, Consolas,Lucida Console, monospace;
+                color: azure;
+                text-align: center;
+            }
+        </style>
+    <title>Home</title>
+    <meta charset="UTF-8">
+    </head>
+
+    <body>
+
+        <h1 style="font-family:monaco, Consolas,Lucida Console, monospace;color: azure;" >
+            <center>
+                <img src=eyes.png width="50px" height="50px">  Are you looking for someone?  <img src=eyes.png width="50px" height="50px">
+            </center>
+        </h1>
+        <div id="block_container">
+                <div id="bloc1">
+                    <h3>Nat thitimathanyakul</h3>
+                    <a href="jam.html" target="_blank">
+                    <img src="https://scontent.fbkk12-4.fna.fbcdn.net/v/t1.0-9/117643924_1227860577563870_5915696988648966165_o.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeG6_1XdUhs4YFD_S-U05Raw6Bpmo9iCm1joGmaj2IKbWDEHOB4BgDxlQrppNfd3iT5iOwnp2XIXSqK30JhRbcHH&_nc_ohc=UaKNtkxFbxEAX8lvvc6&_nc_ht=scontent.fbkk12-4.fna&oh=3af09c15b8a766f2cbb8794cafda3900&oe=6007B559" width="150" height="150" >
+                    </a>
+                </div>
+                <div id="bloc1">
+                    <h3>Rattamet Boonwong</h3>
+                    <a href="tontan.html" target="_blank">
+                    <img src="https://scontent.fbkk17-1.fna.fbcdn.net/v/t31.0-8/28827586_1513477365388709_7827976374634047616_o.jpg?_nc_cat=109&ccb=2&_nc_sid=174925&_nc_eui2=AeGMBIUpfH_TFSAnA_6nwizOg2oPOrVcjM-Dag86tVyMz3SiUAStAJzNUQ7CS99n7tKxOsFWRaMFvVXg1sYg50FL&_nc_ohc=lSJum5l0becAX_HhWDk&_nc_ht=scontent.fbkk17-1.fna&oh=2bbbbbe023f017d2903627f150d600ad&oe=60055014" width="150" height="150" >
+                    </a>
+                </div>
+        </div>>
+        <h4 style="font-family:monaco, Consolas,Lucida Console, monospace;color: azure;">
+            <center>
+                !!click at a photo to see personal information!!
+            </center></h4>
+        <div id="block_container">
+            <div id="bloc1">
+                <h3>Narawich Theppawong</h3>
+                <a href="ken.html" target="_blank">
+                <img src="ken.png" width="150" height="150">
+            </a>
+            </div>
+            <div id="bloc1">
+                <h3>Songpon Chomchuen</h3>
+                <a href="ing.html" target="_blank">
+                <img src="https://scontent.fbkk12-2.fna.fbcdn.net/v/t1.0-9/121613781_1500631770143037_476755680366062785_n.jpg?_nc_cat=105&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeFQ-bSTNMoSBuJPG_FB-asebod4rOAw1bBuh3is4DDVsJ-KQVT0r8EYnxWUD-TR7Tyou_EA-Vuer2RvoY4izcdG&_nc_ohc=X9_SrEHvlaUAX9wYdpJ&_nc_oc=AQkAJxOQVgWsTvEArpS3zga965RSw1OzhuQprJe5nzoEYcheOfjCwnm3sYVIg1ZBrrgzajLpP40fk4WmAt0Z7lNi&_nc_ht=scontent.fbkk12-2.fna&oh=78e3a0b7ad5c4a63f89d7135901e7066&oe=60067201" width="150" height="150" >
+            </a>
+            </div>
+    </div>>
+
+    </body>
+</html>
